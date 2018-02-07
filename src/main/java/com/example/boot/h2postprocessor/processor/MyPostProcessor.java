49bde86e2ec7ac87bbab76c235a83b1b542487c7
cdc895642 <cdc895642@hotmail.com>
2018-02-07 23:07:04 +0200

    init commit

---
+++ src/main/java/com/example/boot/h2postprocessor/processor/MyPostProcessor.java
@@ -0,0 +1,47 @@
+package com.example.boot.h2postprocessor.processor;
+
+import com.example.boot.h2postprocessor.entity.Student;
+import java.lang.reflect.Field;
+import java.sql.ResultSet;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+@Component
+public class MyPostProcessor implements BeanPostProcessor {
+
+  @Override
+  public Object postProcessBeforeInitialization(Object bean, String s) throws BeansException {
+//    Class type = bean.getClass();
+    Field[] fields = bean.getClass().getDeclaredFields();
+    for (Field field : fields) {
+      MyAnnotation myAnnotation = field.getAnnotation(MyAnnotation.class);
+      if (myAnnotation != null) {
+        field.setAccessible(true);
+//        Object value = ReflectionUtils.getField(field, bean);
+        ReflectionUtils.setField(field, bean, createRowMapper(bean));
+      }
+    }
+    return bean;
+  }
+
+  private Object createRowMapper(Object bean) {
+
+    RowMapper rowMapper = (resultSet, rowNum) -> {
+      Student student = new Student();
+      student.setId(resultSet.getLong("id"));
+      student.setName(resultSet.getString("name"));
+      student.setPassportNumber(resultSet.getString("passport_number"));
+      return student;
+    };
+    return rowMapper;
+
+  }
+
+  @Override
+  public Object postProcessAfterInitialization(Object bean, String s) throws BeansException {
+    return bean;
+  }
+}
